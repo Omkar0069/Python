95f764e1dd651a96a9cc66df21ce0fef089cbb1e
Omkar0069 <parabomkar2006@gmail.com>
2025-09-18 15:35:27 +0530

    added some details...

--- module.py
+++ module.py
@@ -1,3 +1,8 @@
 import pyjokes
+print("Printing Jokes....")
 joke = pyjokes.get_joke()
 print(joke)
+
+# Types of Module: (ctrl + / to comment)
+# 1.Built in Module(already install in python)
+# 2.External Module(Installed using pip)
