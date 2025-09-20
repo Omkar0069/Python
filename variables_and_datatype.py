107ee8d66d7331c7e566b77e529ff9d95acd61f3
Omkar0069 <parabomkar2006@gmail.com>
2025-09-20 10:05:26 +0530

    Datatypes and Variable explained in short

---
+++ variables_and_datatype.py
@@ -0,0 +1,9 @@
+a = 1  # integer Variable stored in variable a
+b = 2  # integer Variable stored in variable b
+c = "Omkar"  # String Variable stored in variable c
+d = 1.34  # Floating Variable stored in variable d
+# Here integer, string, and floating is datatype. Another datatypes include boolean(output is either true or false) and none(to mark nothing)
+
+
+print(a + b)
+print(c)
