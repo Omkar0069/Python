0cb31b078ad1baad010c9c76996920ed95214a5d
Omkar0069 <parabomkar2006@gmail.com>
2025-09-20 11:55:44 +0530

    Finish merge

---
+++ Type.py
@@ -0,0 +1,17 @@
+a = 34
+t = type(a)
+print(t)
+
+b = 34.343
+z = type(b)
+print(z)
+
+c = "Omkar"
+y = type(c)
+print(y)
+
+# We can convert the type of the variable only when the condition is valid
+d = "34.45"
+e = float(d)
+x = type(d)
+print(type(e))
