d37b087b24c21f3e8f9277de9d750e810a725b94
Omkar0069 <parabomkar2006@gmail.com>
2025-09-20 10:26:54 +0530

    Rules

---
+++ rules.py
@@ -0,0 +1,5 @@
+a = 1 #right can contain only character, number and underscore
+b = 2 #right can start with only character and underscore
+c = 34 #right cant start with digit
+_Omkar= 43 #right no while space is allowed inside a variable name
+@_Omkar = 343 #wrong  Variable name cannot contain special character
