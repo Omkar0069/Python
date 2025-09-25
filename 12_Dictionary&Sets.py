69c70a4dfe3d43cd0976fc1449c12082ba249777
Omkar0069 <parabomkar2006@gmail.com>
2025-09-25 19:46:39 +0530

    added properties

---
+++ 12_Dictionary&Sets.py
@@ -0,0 +1,15 @@
+marks = {
+    "Omkar": 100,
+    "Harshal": 96,
+    "Aaron": 95,
+    "Sumeet": 98,
+}
+# print(marks, type(marks))
+print(marks ["Omkar"])
+
+
+#PROPERTIES:
+# It is Unordered,
+# It is Mutable,
+# It is Indexed,
+# Cannot contain duplicate keys
