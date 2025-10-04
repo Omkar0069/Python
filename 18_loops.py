3073dc186440431aab2d1464003d75cae1d7a910
Omkar0069 <parabomkar2006@gmail.com>
2025-10-04 14:37:02 +0530

    loops wrapup

---
+++ 18_loops.py
@@ -0,0 +1,56 @@
+i = 0
+# for i in range(1,6):
+#     print(i)
+
+# While Loops:
+
+
+# while(i<=50):
+#     print(i)
+#     i+=1 # i = i + 1
+
+# List using While Loop
+
+# l = [1, "Omkar", 45, False]
+# while(i<len(l)):
+#     print(l[i])
+#     i+=1
+# print("Loop Ended")
+
+#For Loops
+
+# for i in range(0,50,2): -> (Start, Stop, StepSize)
+    # print(i)
+    # i+=1
+
+# For Loop using string
+# s = "Omkar"
+# for i in s:
+#     print(i)
+
+# For Loop with else
+# l = [1, 2,5,6,7,8,4]
+# for item in l:
+#     print(item)
+# else:
+#     print("Done") # This prints when loop exhausts!
+
+# Break and continue in loop
+# for i in range(100):
+#     if i == 69:
+#         break # Exits the loop right now
+#     print(i)
+
+# for i in range(100):
+#     if i == 69:
+#         continue # Skips this iteration (wont print 69)
+#     print(i)
+
+# Pass in Loop
+
+for i in range(100):
+    pass # Skip the loop, instruct to do nothing
+
+while(i<199):
+    print(i)
+    i+=1
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-#PROBLEM 1:
+for i in range(100):
+#     if i == 69:
+#         break # Exits the loop right now
+#     print(i)
